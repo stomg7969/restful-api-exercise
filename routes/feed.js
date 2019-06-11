2d70581ad0e7b388a31084e9310adfc82d029803
stomg7969 <stomg7969@gmail.com>
2019-06-11 16:42:02 -0400

    Added auth middle where to authenticate before authorizing with jwt

--- routes/feed.js
+++ routes/feed.js
@@ -4,13 +4,15 @@ const router = express.Router();
 const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 // All routes start with /feed/
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
 router.post(
   '/post',
+  isAuth,
   [
     body('title')
       .trim()
@@ -23,7 +25,9 @@ router.post(
 );
 
 // put / patch
-router.put('/post/:postId',
+router.put(
+  '/post/:postId',
+  isAuth,
   [
     body('title')
       .trim()
@@ -34,6 +38,6 @@ router.put('/post/:postId',
   ],
   feedController.updatePost
 );
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
