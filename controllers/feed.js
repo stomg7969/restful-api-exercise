2755165444eb1879e1908b91c90574a6db572bad
stomg7969 <stomg7969@gmail.com>
2019-06-11 12:26:37 -0400

    Installed mongoose, added express-validator for server side validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator/check');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [{
@@ -13,6 +15,13 @@ exports.getPosts = (req, res, next) => {
   });
 };
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: 'Validation Failed',
+      errors: errors.array()
+    });
+  }
   // req.body is coming from body parser.
   const title = req.body.title;
   const content = req.body.content;
--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+// express-validator --> npm install --save express-validator
+const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
 
 // All routes start with /feed/
 router.get('/posts', feedController.getPosts);
-router.post('/post', feedController.createPost);
+router.post(
+  '/post',
+  [
+    body('title')
+      .trim()
+      .isLength({ min: 5 }),
+    body('content')
+      .trim()
+      .isLength({ min: 5 })
+  ],
+  feedController.createPost);
 
 module.exports = router;
