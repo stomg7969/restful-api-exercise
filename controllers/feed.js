be5eb10740e44ac03217e3e846660d0d6384a116
stomg7969 <stomg7969@gmail.com>
2019-06-11 19:11:52 -0400

    Refactored to async await for feed.js controller

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,22 +4,28 @@ const path = require('path');
 
 const Post = require('../models/post');
 const User = require('../models/user');
-
-exports.getPosts = (req, res, next) => {
+// Async Await way instead of using .then()
+exports.getPosts = async (req, res, next) => {
   // if req.query.page is undefined, set to 1 as default.
   const currentPage = req.query.page || 1;
   const numberOfPostsPerPage = 2;
+  /*
   let totalItems;
   Post.find()
     .estimatedDocumentCount()
     .then(count => {
       totalItems = count;
       return Post.find()
-        .skip((currentPage - 1) * numberOfPostsPerPage) // If I am on first page, then it becomes 0, so no skipping. if page 2, then skip that amount.
-        .limit(numberOfPostsPerPage); // limit the amount rendered.
+        .skip((currentPage - 1) * numberOfPostsPerPage)
+        .limit(numberOfPostsPerPage);
     })
     .then(posts => {
-      res.status(200).json({ posts, totalItems });
+      res.status(200)
+        .json({
+          message: 'Fetched posts successfully.',
+          posts: posts,
+          totalItems: totalItems
+        });
     })
     .catch(err => {
       if (!err.statusCode) {
@@ -27,26 +33,42 @@ exports.getPosts = (req, res, next) => {
       }
       next(err);
     });
+  */
+
+  try {
+    const totalItems = await Post.find().estimatedDocumentCount;
+    const posts = await Post.find()
+      .populate('creator')
+      .skip((currentPage - 1) * numberOfPostsPerPage) // If I am on first page, then it becomes 0, so no skipping. if page 2, then skip that amount.
+      .limit(numberOfPostsPerPage); // limit the amount rendered.
+
+    res.status(200).json({ posts, totalItems });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+
+  }
 };
-exports.getPost = (req, res, next) => {
+exports.getPost = async (req, res, next) => {
   const { postId } = req.params;
-  Post.findById(postId)
-    .then(post => {
-      if (!post) {
-        const error = new Error('No post found');
-        error.statusCode = 404;
-        throw error;
-      }
-      res.status(200).json({ message: 'Success', post });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+  try {
+    const post = await Post.findById(postId)
+    if (!post) {
+      const error = new Error('No post found');
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({ message: 'Success', post });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
-exports.createPost = (req, res, next) => {
+exports.createPost = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error('Validation Failed');
@@ -74,31 +96,29 @@ exports.createPost = (req, res, next) => {
   // by storing it, formData.append() will allow us to append data to the object.
   // lastly, when fetching, formData will automatically set the headers.
   // Also, body just becomes formData. (no Stringify.)
-  post.save()
-    .then(r => User.findById(req.userId))
-    .then(user => {
-      creator = user; // made it global scope to use it in the other function.
-      user.posts.push(post);
-      return user.save();
-    })
-    .then(r => {
-      res.status(201).json({
-        message: 'Post created successfully!',
-        post: post,
-        creator: {
-          _id: creator._id,
-          name: creator.name
-        }
-      });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
+  try {
+    await post.save();
+    const user = await User.findById(req.userId);
+    user.posts.push(post);
+    await user.save();
+
+    res.status(201).json({
+      message: 'Post created successfully!',
+      post: post,
+      creator: {
+        _id: user._id,
+        name: user.name
       }
-      next(err);
     });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+
+  }
 };
-exports.updatePost = (req, res, next) => {
+exports.updatePost = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error('Validation failed');
@@ -118,73 +138,71 @@ exports.updatePost = (req, res, next) => {
     error.statusCode = 422;
     throw error;
   }
-  Post.findById(postId)
-    .then(post => {
-      if (!post) {
-        const error = new Error('No file picked.');
-        error.statusCode = 404;
-        throw error;
-      }
-      if (post.creator.toString() !== req.userId) {
-        // post.creator is ObjectId. so It returns id, but I can still get an access like name and email and so on.
-        const error = new Error('Not authorized.');
-        error.statusCode = 403;
-        throw error;
-      }
-      if (imageUrl !== post.imageUrl) {
-        // If image is newly uploaded, it will not be the same as post.imageUrl.
-        // so, it will trigger this function whenever I upload a new image. Then delete the old image.
-        clearImage(post.imageUrl);
-      }
-      post.title = title;
-      post.imageUrl = imageUrl;
-      post.content = content;
-      return post.save();
-    })
-    .then(r => {
-      res.status(200).json({ post: r });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
-};
-exports.deletePost = (req, res, next) => {
-  const { postId } = req.params;
-  Post.findById(postId)
-    .then(post => {
-      if (!post) {
-        const error = new Error('No file picked.');
-        error.statusCode = 404;
-        throw error;
-      }
-      if (post.creator.toString() !== req.userId) {
-        // post.creator is ObjectId. so It returns id, but I can still get an access like name and email and so on.
-        const error = new Error('Not authorized.');
-        error.statusCode = 403;
-        throw error;
-      }
-      // Check if it's the right user --> delete post --> delete image.
+  try {
+    const post = await Post.findById(postId)
+    if (!post) {
+      const error = new Error('No file picked.');
+      error.statusCode = 404;
+      throw error;
+    }
+    if (post.creator.toString() !== req.userId) {
+      // post.creator is ObjectId. so It returns id, but I can still get an access like name and email and so on.
+      const error = new Error('Not authorized.');
+      error.statusCode = 403;
+      throw error;
+    }
+    if (imageUrl !== post.imageUrl) {
+      // If image is newly uploaded, it will not be the same as post.imageUrl.
+      // so, it will trigger this function whenever I upload a new image. Then delete the old image.
       clearImage(post.imageUrl);
-      return Post.findByIdAndRemove(postId);
-    })
-    .then(r => {
-      return User.findById(req.userId);
-    })
-    .then(user => {
-      user.posts.pull(postId); // Reason for this line is to also delete association. Before this when deleting post, 
-      // ... it deletes posts, but not the id that is associated to the user.
-      return user.save();
-    })
-    .then(r => res.status(200).json({ message: 'Post deleted' }))
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+    }
+    post.title = title;
+    post.imageUrl = imageUrl;
+    post.content = content;
+
+    const result = await post.save();
+
+    res.status(200).json({ post: result });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+exports.deletePost = async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    const post = await Post.findById(postId)
+
+    if (!post) {
+      const error = new Error('No file picked.');
+      error.statusCode = 404;
+      throw error;
+    }
+    if (post.creator.toString() !== req.userId) {
+      // post.creator is ObjectId. so It returns id, but I can still get an access like name and email and so on.
+      const error = new Error('Not authorized.');
+      error.statusCode = 403;
+      throw error;
+    }
+    // Check if it's the right user --> delete post --> delete image.
+    clearImage(post.imageUrl);
+    await Post.findByIdAndRemove(postId); // No need to store if not going to use it.
+
+    const user = await User.findById(req.userId);
+
+    user.posts.pull(postId); // Reason for this line is to also delete association. Before this when deleting post, 
+    // ... it deletes posts, but not the id that is associated to the user.
+    await user.save();
+
+    res.status(200).json({ message: 'Post deleted' })
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 const clearImage = filePath => {
