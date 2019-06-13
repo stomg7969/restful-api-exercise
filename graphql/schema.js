ea457f7132c31112dea7fd69818726d27770747c
stomg7969 <stomg7969@gmail.com>
2019-06-13 13:20:28 -0400

    User can see status and update status

--- graphql/schema.js
+++ graphql/schema.js
@@ -39,16 +39,18 @@ module.exports = buildSchema(`
     imageUrl: String!
   }
 
+  type RootQuery {
+    login(email: String!, password: String!): AuthData!
+    posts(page: Int): PostData!
+    post(id: ID!): Post!
+    user: User!
+  }
   type RootMutation {
     createUser(userInput: UserInputData): User!
     createPost(postInput: PostInputData): Post!
     updatePost(id: ID!, postInput: PostInputData): Post!
     deletePost(id: ID!): Boolean
-  }
-  type RootQuery {
-    login(email: String!, password: String!): AuthData!
-    posts(page: Int): PostData!
-    post(id: ID!): Post!
+    updateStatus(status: String!): User!
   }
   schema {
     query: RootQuery
